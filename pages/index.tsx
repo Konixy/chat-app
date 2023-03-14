039c438dc4ecafacbb34e378bb7b49b6366bfcca
Konixy <konixy.p@gmail.com>
2023-03-14 10:45:56 +0000

    Update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { signIn, signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { Button } from '@nextui-org/react';
+import { Button, Loading } from '@nextui-org/react';
 
 export default function Home() {
   const { data } = useSession();
@@ -10,7 +10,7 @@ export default function Home() {
     console.log(data);
   }, [data]);
   return (
-    <div>
+    <div className="my-20 flex flex-row justify-center text-center">
       {data ? (
         <>
           <Link href="app">My messages</Link>
@@ -18,6 +18,10 @@ export default function Home() {
             Log out
           </Button>
         </>
+      ) : data === undefined ? (
+        <Button color="primary" disabled className="px-4">
+          <Loading color="currentColor" size="sm" />
+        </Button>
       ) : (
         <Button className="bg-blue-500" icon={<i className="fab fa-google" />} as="button" onClick={() => signIn('google')} color="primary" auto>
           Sign in with Google
