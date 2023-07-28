1a202d974a41899eb0ca91793011474c9cf7fa1e
Konixy <konixy.p@gmail.com>
2023-07-28 17:11:30 +0200

    Update

--- components/Chat/Feed/Messages/Input.tsx
+++ components/Chat/Feed/Messages/Input.tsx
@@ -1,16 +1,24 @@
 import { Session } from 'next-auth';
 import React, { useState } from 'react';
+import toast from 'react-hot-toast';
 
 export default function MessageInput({ session, conversationId }: { session: Session; conversationId: string }) {
   const [body, setBody] = useState('');
 
+  async function onSendMessage(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (error: any) {
+      console.log('onSendMessage ERROR', error);
+      toast.error(error.message);
+    }
+  }
+
   return (
     <div className="w-full px-4 py-6">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-        }}
-      >
+      <form onSubmit={onSendMessage}>
         <input
           type="text"
           value={body}
